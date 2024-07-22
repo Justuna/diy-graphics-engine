5b89f47410b52f8e7ee36eed537b1343c565c10f
Justuna <jlpisa999@yahoo.com>
2024-07-22 19:59:04 -0400

    switched to using sdl

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # diy-graphics-engine
-A DIY graphics engine made in Rust for the purpose of learning about rendering and graphics programming. Based off of [this tutorial series.](https://www.youtube.com/watch?v=ih20l3pJoeU)
+A DIY 3D graphics engine made in Rust for the purpose of learning about rendering, graphics programming, and common graphics libraries/specs. 
+
+Based mostly off of the following tutorial series:
+- [Code-It-Yorself! 3D Graphics Engine](https://www.youtube.com/watch?v=ih20l3pJoeU)
+- [Rust and OpenGL from scratch](https://nercury.github.io/rust/opengl/tutorial/2018/02/08/opengl-in-rust-from-scratch-00-setup.html)
--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,30 @@
-use std::error::Error;
-
-use winit::{event, event_loop};
+use std::{error::Error};
+use sdl2::{event::Event};
 
 pub mod app;
 
 pub fn run() -> Result<(), Box<dyn Error>>
 {
-    app::EngineHandler::start()?;
+    let sdl_context = sdl2::init()?;
+    let video_subsystem = sdl_context.video()?;
+    let window = video_subsystem
+        .window("DIY Graphics Engine", 800, 600)
+        .position_centered()
+        .build()?;
 
+    let mut event_pump = sdl_context.event_pump().unwrap();
+
+    'running: loop 
+    {
+        for event in event_pump.poll_iter() 
+        {
+            match event 
+            {
+                Event::Quit {..} => break 'running,
+                _ => {},
+            }
+        }
+    }
+    
     return Ok(());
 }
