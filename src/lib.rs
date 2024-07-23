d37bbd64c74406e94117867dfc8571cdea30c2ea
Justuna <jlpisa999@yahoo.com>
2024-07-22 21:06:31 -0400

    switched libs again to glfw

--- src/lib.rs
+++ src/lib.rs
@@ -1,26 +1,26 @@
 use std::{error::Error};
-use sdl2::{event::Event};
-
+use glfw::{Action, Context, Key};
 pub mod app;
 
 pub fn run() -> Result<(), Box<dyn Error>>
 {
-    let sdl_context = sdl2::init()?;
-    let video_subsystem = sdl_context.video()?;
-    let window = video_subsystem
-        .window("DIY Graphics Engine", 800, 600)
-        .position_centered()
-        .build()?;
+    let mut glfw = glfw::init_no_callbacks()?;
+    let (mut window, events) = glfw
+        .create_window(
+            600, 
+            900, 
+            "DIY Graphics Engine", 
+            glfw::WindowMode::Windowed
+        ).ok_or(glfw::Error::NoWindowContext)?;
 
-    let mut event_pump = sdl_context.event_pump().unwrap();
+    window.set_key_polling(true);
+    window.make_current();
 
-    'running: loop 
-    {
-        for event in event_pump.poll_iter() 
-        {
-            match event 
-            {
-                Event::Quit {..} => break 'running,
+    while !window.should_close() {
+        glfw.poll_events();
+        for (_, event) in glfw::flush_messages(&events) {
+            match event {
+                glfw::WindowEvent::Close => window.set_should_close(true),
                 _ => {},
             }
         }
